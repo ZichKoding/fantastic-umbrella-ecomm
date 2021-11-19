33960ce4b9d4fa5f717d62e81ffc391c2226260d
Chris Zichko <chriszichkocoding@gmail.com>
2021-11-19 09:21:48 -0500

    Added create method to the category endpoint

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -50,12 +50,17 @@ router.get('/:id', (req, res) => {
       console.log(err);
       res.json(500).json(err);
     });
-  // find one category by its `id` value
-  // be sure to include its associated Products
 });
 
 router.post('/', (req, res) => {
-  // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  })
+    .then(dbCategoryData => res.json(dbCategoryData))
+    .catch(err => {
+      console.log(err);
+      res.json(500).json(err);
+    })
 });
 
 router.put('/:id', (req, res) => {
